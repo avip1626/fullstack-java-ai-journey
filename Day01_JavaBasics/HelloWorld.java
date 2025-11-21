25b2a342171b5e824a8b5221ab9f265ef5bc5740
Avinash Pandey <avinashcode9293@gmail.com>
2025-11-21 18:51:49 +0530

    Add HelloWorld program

---
+++ Day01_JavaBasics/HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld {
+    public static void main (String[] args){
+    System.out.println("Hellow world,I am Avinash, this journey now for Full stck Java Developer  ");
+    }
+}
